2f0c304557248be3b8584045d56384340be2b881
Martin874 <martinrivera8744@gmail.com>
2024-11-18 19:19:25 -0300

    error del vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css', // Corrección aquí
                 'resources/js/app.js',
             ],
             refresh: true,
